cdb110a70164109f662a00cfeac988fba92112b3
Jarancibia <arancibiajav@gmail.com>
2026-02-18 23:38:14 +0100

    feat: Add route to download backups

--- src/routes/backup.routes.js
+++ src/routes/backup.routes.js
@@ -5,6 +5,7 @@ const ctrl = require('../controllers/backup.controller');
 router.get('/', ctrl.listBackups);
 router.get('/status', ctrl.getStatus);
 router.post('/trigger/:targetId', ctrl.triggerBackup);
+router.get('/download/:targetId/:filename', ctrl.downloadBackup);
 router.delete('/:targetId/:filename', ctrl.deleteBackup);
 
 module.exports = router;
